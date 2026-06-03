b5222b574fa54bfed8457547aed3bf1989411025
northernhills92-oss <northernhills92@gmail.com>
2026-06-03 19:08:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 import streamlit as st
-from data import market
+from data.market import get_usdt_pairs
 
-st.title("DEBUG")
-
-st.write("Market module:", market.__file__)
-
-pairs = market.get_usdt_pairs()
+pairs = get_usdt_pairs()
 
 st.write("Pairs found:", len(pairs))
-
-if pairs:
-    st.write(pairs[:10])
